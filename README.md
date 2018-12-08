fb9348b1c49a2dcb1c2b695ad884aa1446b7e022
Weerapitch Panlertkitsakul <weerapitch@gmail.com>
2018-12-08 15:55:29 -0500

    Put code for all components together Updated README.MD

--- README.md
+++ README.md
@@ -1,11 +1,29 @@
-# PetDetectionFeeder
-Autonomous pet feeder that uses ARM Mbed and Raspberry Pi #3
+# Pet Detection Feeder
+#### Georgia Institute Of Technology
+#### ECE 4180 Fall 2018
+#### Contributors: Yassine Bennani, Andrew Taira, Landan Seguin, Weerapitch Panlertkitsakul
 
-# Hardware used
-. Arm Mbed \n
-. Raspberry Pi 3 
-. 2x Pressure sensor
-. Water pump
-. 2x Servo motors
-. Water sensor
-. Raspberry Pi Camera
+## Project Summary
+
+Autonomous pet feeder that uses ARM Mbed and Raspberry Pi 3. This feeder dispenses 2 different types of pet food for 2 different species of pets and water into a water bowl. Using a Raspberry Pi Camera, the Raspberry Pi runs image recognition using OpenCV to detect when a pet is in front of the feeder. After determining the pet, the Pi sends a signal to the Mbed to dispense the corresponding food and water into the bowls. The Mbed uses force sensitive resistor (FSR) and water level sensors to stop dispensing when the bowls are full.
+
+## Pictures
+
+## Flow/Block/Pin Diagram?
+
+## Hardware Components
+* [Arm Mbed LPC1768](https://os.mbed.com/platforms/mbed-LPC1768/)
+* [Raspberry Pi 3 Model B+](https://www.raspberrypi.org/products/raspberry-pi-3-model-b-plus/)
+* Raspberry Pi 3 Camera
+* [Force Sensitive Resistor (FSR) (2x)](https://os.mbed.com/users/saporiti/notebook/force-sensitive-resistors/)
+* Water Pump
+* Servo Motor (2x)
+* Water Sensor
+
+## Code
+
+```
+PUT CODE HERE
+```
+
+## Video
--- WaterPump.cpp
+++ WaterPump.cpp
@@ -1,27 +1,95 @@
 #include "mbed.h"
+#include "FSR.h"
+#include "Servo.h"
 
-
-DigitalOut myled(LED1);
-PwmOut Ctrl(p25);
 Serial pc(USBTX, USBRX);
-AnalogIn water(p20);
+AnalogIn water_sensor(p20);
+FSR dog_food_sensor(p18, 10); // Pin 18 is used as the AnalogIn pin and a 10k resistor is used as a voltage divider
+FSR cat_food_sensor(p19, 10); // Pin 19 is used as the AnalogIn pin and a 10k resistor is used as a voltage divider
+PwmOut water_pump(p25);
+Servo dog_food_servo(p21);
+Servo cat_food_servo(p22);
+
+const int DOG_SIGNAL = 1;
+const int CAT_SIGNAL = 2;
+const float WATER_HIGH_THRESHOLD = 0.40;
+const float WATER_LOW_THRESHOLD = 0.33;
+const float WATER_PUMP_ON = 7.0;
+const float WATER_PUMP_OFF = 0.0;
+const float CAT_FOOD_THRESHOLD = 200.0;
+const float DOG_FOOD_THRESHOLD = 200.0;
+const float SERVO_CLOSE = 0.0;
+const float SERVO_OPEN = 0.5;
+
+bool dispenseCatFood() {
+    // return true if still filling up food, false otherwise
+    if (cat_food_sensor.readWeight() < CAT_FOOD_THRESHOLD) {
+        cat_food_servo = SERVO_OPEN;
+        return true;
+    } else {
+        cat_food_servo = SERVO_CLOSE;
+        return false;
+    }
+}
+
+bool dispenseDogFood() {
+    // return true if still filling up food, false otherwise
+    if (dog_food_sensor.readWeight() < DOG_FOOD_THRESHOLD) {
+        dog_food_servo = SERVO_OPEN;
+        return true;
+    } else {
+        dog_food_servo = SERVO_CLOSE;
+        return false;
+    }
+}
+
+bool dispenseWater() {
+    // return true if still filling up water, false otherwise
+    float water = (float) water_sensor;
+    // for debugging:
+    // pc.printf("water level:  %9f\n\r", water);
+    if (water > WATER_HIGH_THRESHOLD) {
+        // if water level is too high, turn off pump
+        water_pump = WATER_PUMP_OFF;
+        return false;
+    } else if (water < WATER_LOW_THRESHOLD) {
+        // if water level is too low, turn on pump
+        water_pump = WATER_PUMP_ON;
+        return true;
+    } else {
+        // if water level is between high and low, turn off pump
+        water_pump = WATER_PUMP_OFF;
+        return false;
+    }
+}
+
 int main() {
-    float wat;
-    while(1) {
-        wat = (float) water;
-        pc.printf("water level:  %9f\n\r",wat);
-        if(wat>0.40){
-            myled= 1;
-            Ctrl =0;
-            }
-        else if(wat<0.33){
-            myled=0;
-            Ctrl = 7;
-            }
-        else {
-            Ctrl = 0;
+    char signal;
+    while (1) {
+        signal = 0;
+        // read in signal from Pi 3
+        if (pc.readable()) {
+            signal = pc.getc();
         }
-        wait(0.3);
-       
+
+        // keep looping if no signal
+        if (signal != 0) {
+            bool water_is_filling = true;
+            bool food_is_filling = true;
+            while (water_is_filling && food_is_filling) {
+                // call function to dispense water
+                water_is_filling = dispenseWater();
+
+                // dispense type of food depending on signal from Pi 3
+                if (signal == DOG_SIGNAL) {
+                    // dog
+                    food_is_filling = dispenseDogFood();
+                } else if (signal == CAT_SIGNAL) {
+                    // cat
+                    food_is_filling = dispenseCatFood();
+                }
+            }
+            wait(10);
+        }
     }
 }
