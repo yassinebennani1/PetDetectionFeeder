b43e6cbb02f4297e0bbf7c39f8f0453225f3cbaf
Weerapitch Panlertkitsakul <weerapitch@gmail.com>
2018-12-10 21:27:06 -0500

    Modify code to accept signals

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 
 ## Project Summary
 
-Autonomous pet feeder that uses ARM Mbed and Raspberry Pi 3. This feeder dispenses 2 different types of pet food for 2 different species of pets and water into a water bowl. Using a Raspberry Pi Camera, the Raspberry Pi runs image recognition using OpenCV to detect when a pet is in front of the feeder. After determining the pet, the Pi sends a signal to the Mbed to dispense the corresponding food and water into the bowls. The Mbed uses force sensitive resistor (FSR) and water level sensors to stop dispensing when the bowls are full.
+Autonomous pet feeder that uses ARM Mbed and Raspberry Pi 3. This feeder dispenses 2 different types of pet food for 2 different species of pets and water into a water bowl. Using a Raspberry Pi Camera, the Raspberry Pi runs image recognition using OpenCV to detect when a pet is in front of the feeder. After determining the pet, the Pi sends a signal to the Mbed to dispense the corresponding food and water into the bowls. The Mbed uses sonar and water level sensors to stop dispensing when the bowls are full.
 
 ## Pictures
 
@@ -15,7 +15,7 @@ Autonomous pet feeder that uses ARM Mbed and Raspberry Pi 3. This feeder dispens
 * [Arm Mbed LPC1768](https://os.mbed.com/platforms/mbed-LPC1768/)
 * [Raspberry Pi 3 Model B+](https://www.raspberrypi.org/products/raspberry-pi-3-model-b-plus/)
 * Raspberry Pi 3 Camera
-* [Force Sensitive Resistor (FSR) (2x)](https://os.mbed.com/users/saporiti/notebook/force-sensitive-resistors/)
+* [Sonar Sensor (2x)]()
 * Water Pump
 * Servo Motor (2x)
 * Water Sensor
--- main.cpp
+++ main.cpp
@@ -113,20 +113,22 @@ Servo dogServo(p22);
 DigitalOut myled(LED1);
 DigitalOut myled2(LED2);
 PwmOut Ctrl(p25);
-Serial pc(USBTX, USBRX);
+RawSerial  pi(USBTX, USBRX);
 AnalogIn water(p20);
 
-DigitalOut trigger(p10);
-DigitalIn  echo(p12);
-int distance = 0;
-int correction = 0;
-Timer sonar;
+// dog sonar initializations
+DigitalOut dogTrigger(p10);
+DigitalIn  dogEcho(p12);
+int dogSonarDistance = 0;
+int dogSonarCorrection = 0;
+Timer dogSonarTimer;
 
-DigitalOut trigger1(p6);
-DigitalIn  echo1(p8);
-int distance1 = 0;
-int correction1 = 0;
-Timer sonar1;
+// cat sonar initializations
+DigitalOut catTrigger(p6);
+DigitalIn  catEcho(p8);
+int catSonarDistance = 0;
+int catSonarCorrection = 0;
+Timer catSonarTimer;
 
 int dist1=0;
 int dist2=0;
@@ -137,161 +139,176 @@ int dist5=0;
 int dist6=0;
 float wat;
 bool cond;
-bool dogSensed = true;
-bool catSensed = true;
-bool waterSensed = dogSensed || catSensed;
-    
+bool dogSensed = false;
+bool catSensed = false;
+bool waterSensed = false;
+
+void fillDogFood() {
+    printf("dooooooooooog!!!!!!");
+    // dogTrigger dogSonarTimer to send a ping
+    dogTrigger = 1;
+    myled = 1;
+    myled2 = 0;
+    dogSonarTimer.reset();
+    wait_us(10.0);
+    dogTrigger = 0;
+    myled = 0;
+    //wait for dogEcho high
+    while (dogEcho==0) {};
+    myled2=dogEcho;
+    //dogEcho high, so start timer
+    dogSonarTimer.start();
+    //wait for dogEcho low
+    while (dogEcho==1) {};
+    //stop timer and read value
+    dogSonarTimer.stop();
+    //subtract software overhead timer delay and scale to cm
+    dogSonarDistance = (dogSonarTimer.read_us()-dogSonarCorrection)/58.0;
+    myled2 = 0;
+    dist1=dist2;
+    dist2=dist3;
+    dist3 = dogSonarDistance;
+    printf(" %d dog cm \n\r",dogSonarDistance);
+    if(dist1 >= 50 && dist2>=50 && dist3 >=50){
+        dogSensed = false;
+        printf("-------------------------------------------------");
+        break;
+    }
+    if(dogSonarDistance>=0 && dogSonarDistance <=40){
+        if(dogSonarDistance <= 40 && dogSonarDistance >= 23){
+            dogServo = 0.3;
+            wait(0.3);
+            dogServo = 1.0;
+            wait(0.7);
+        }
+    }
+    //wait so that any dogEcho(s) return before sending another ping
+    wait(0.2);
+}
+
+void fillCatFood() {
+    printf("caaaaaaaaaaaaat!!!!!!");
+    // trigger sonar to send a ping
+    catTrigger = 1;
+    catSonarTimer.reset();
+    wait_us(10.0);
+    catTrigger = 0;
+    //wait for echo high
+    while (catEcho==0) {};
+    //echo high, so start timer
+    catSonarTimer.start();
+    //wait for echo low
+    while (catEcho==1) {};
+    //stop timer and read value
+    catSonarTimer.stop();
+    //subtract software overhead timer delay and scale to cm
+    catSonarDistance = (catSonarTimer.read_us()-correction)/58.0;
+    dist4=dist5;
+    dist5=dist6;
+    dist6 = catSonarDistance;
+    printf(" %d cat cm \n\r",catSonarDistance);
+    if((dist4 >= 41 && dist5>=41 && dist6 >=41) || (dist4 <= 22 && dist5 <=22 && dist6 <=22)){
+        printf("-------------------------------------------------");
+        catSensed = false;
+        break;
+    }
+    if(catSonarDistance>=0 && catSonarDistance <=40){
+        if(catSonarDistance <= 35 && catSonarDistance >= 23){
+            catServo = 0.5;
+            wait(0.3);
+            catServo = 0.0;
+            wait(0.7);
+        }
+    }
+    //wait so that any echo(s) return before sending another ping
+    wait(0.2);
+}
+
+void fillWater() {
+    wat = (float) water;
+    if(wat >= 0.45){
+        Ctrl = 0;
+        waterSensed = false;
+    } else if(wat<0.20){
+        Ctrl = 7;
+    } else{
+        Ctrl = 0;
+        waterSensed = false;
+    }
+    wait(0.3);
+}
+
+void dev_recv()
+{
+   char temp = 0;
+   myled = !myled;
+   while(pi.readable()) {
+       temp = pi.getc();
+       if (temp=='2') {
+           myled2 = 1;
+           catSensed = true;
+       }
+       if (temp=='1') {
+           myled2 = 1;
+           dogSensed = true;
+       }
+   }
+}
+
 int main(){
     catServo = 0.0;
     dogServo = 1.0;
+    pi.baud(9600);
+    pi.attach(&dev_recv, Serial::RxIrq);
     wait(2);
-    
+
     while(1){
-        //dog food
-        if(dogSensed){
-            printf("dooooooooooog!!!!!!");
-            sonar.reset();
-            // measure actual software polling timer delays
-            // delay used later in time correction
-            // start timer
-            sonar.start();
-            // min software polling delay to read echo pin
-            while (echo==2) {};
-            myled2 = 0;
-            // stop timer
-            sonar.stop();
-            // read timer
-            correction = sonar.read_us();
-            printf("Approximate software overhead timer delay is %d uS\n\r",correction);
-            //Loop to read Sonar distance values, scale, and print
-            while(dogSensed) {
-                // trigger sonar to send a ping
-                trigger = 1;
-                myled = 1;
-                myled2 = 0;
-                sonar.reset();
-                wait_us(10.0);
-                trigger = 0;
-                myled = 0;
-                //wait for echo high
-                while (echo==0) {};
-                myled2=echo;
-                //echo high, so start timer
-                sonar.start();
-                //wait for echo low
-                while (echo==1) {};
-                //stop timer and read value
-                sonar.stop();
-                //subtract software overhead timer delay and scale to cm
-                distance = (sonar.read_us()-correction)/58.0;
-                myled2 = 0;
-                dist1=dist2;
-                dist2=dist3;
-                dist3 = distance;
-                printf(" %d dog cm \n\r",distance);
-                if(dist1 >= 50 && dist2>=50 && dist3 >=50){
-                    dogSensed = false;
-                    printf("-------------------------------------------------");
-                    break;
-                }
-                if(distance>=0 && distance <=40){
-                    if(distance <= 40 && distance >= 23){
-                        dogServo = 0.3;
-                        wait(0.3);
-                        dogServo = 1.0;
-                        wait(0.7); 
-                    }   
-                }
-                //wait so that any echo(s) return before sending another ping
-                wait(0.2);
-            }
+        dogSensed = false;
+        catSensed = false;
+        // check for signals from Pi
+        while(!dogSensed && !catSensed) {
+            sleep();
         }
 
+        // dog
+        if (dogSensed) {
+            waterSensed = true;
+            dogSonarTimer.reset();
+            dogSonarTimer.start();
+            while (dogEcho==2) {};
+            dogSonarTimer.stop();
+            dogSonarCorrection = dogSonarTimer.read_us();
+            while (dogSensed || waterSensed) {
+                if (dogSensed) {
+                    fillDogFood();
+                }
+
+                if (waterSensed) {
+                    fillWater();
+                }
+            }
+        }
 
         // cat food
-        if (catSensed){
-            printf("caaaaaaaaaaaaat!!!!!!");
-            sonar1.reset();
-            // measure actual software polling timer delays
-            // delay used later in time correction
-            // start timer
-            sonar1.start();
-            // min software polling delay to read echo pin
-            while (echo1==2) {};
-            myled2 = 0;
-            // stop timer
-            sonar1.stop();
-            // read timer
-            correction1 = sonar1.read_us();
-            printf("Approximate software overhead timer delay is %d uS\n\r",correction1);
-            //Loop to read Sonar distance values, scale, and print
-            while(catSensed) {
-                // trigger sonar to send a ping
-                trigger1 = 1;
-                myled = 1;
-                myled2 = 0;
-                sonar1.reset();
-                wait_us(10.0);
-                trigger1 = 0;
-                myled = 0;
-                //wait for echo high
-                while (echo1==0) {};
-                myled2=echo1;
-                //echo high, so start timer
-                sonar1.start();
-                //wait for echo low
-                while (echo1==1) {};
-                //stop timer and read value
-                sonar1.stop();
-                //subtract software overhead timer delay and scale to cm
-                distance1 = (sonar1.read_us()-correction)/58.0;
-                myled2 = 0;
-                dist4=dist5;
-                dist5=dist6;
-                dist6 = distance1;
-                printf(" %d cat cm \n\r",distance1);
-                if((dist4 >= 41 && dist5>=41 && dist6 >=41) || (dist4 <= 22 && dist5 <=22 && dist6 <=22)){
-                    printf("-------------------------------------------------");
-                    catSensed = false;
-                    break;
+        if (catSensed) {
+            waterSensed = true;
+            catSonarTimer.reset();
+            catSonarTimer.start();
+            while (catEcho==2) {};
+            catSonarTimer.stop();
+            catSonarCorrection = catSonarTimer.read_us();
+            while (catSensed || waterSensed) {
+                if (catSensed) {
+                    fillCatFood();
                 }
-                if(distance1>=0 && distance1 <=40){
-                    if(distance1 <= 35 && distance1 >= 23){
-                        catServo = 0.5; 
-                        wait(0.3);
-                        catServo = 0.0; 
-                        wait(0.7);
-                    }
+
+                if (waterSensed) {
+                    fillWater();
                 }
-                //wait so that any echo(s) return before sending another ping
-                wait(0.2);
             }
         }
-        //water filling
-        if(waterSensed){
-            cond = true;
-            while(cond) {
-                wat = (float) water;
-                pc.printf("water level:  %9f\n\r",wat);
-                if(wat>=0.45){
-                    myled= 1;
-                    Ctrl =0;
-                    break;
-                }
-                else if(wat<0.20){
-                    myled=0;
-                    Ctrl = 7;
-                }
-                else{
-                    Ctrl = 0;
-                }
-                wait(0.3);
-            }
-        }
+
+        wait(10);
+        myled2 = 0;
     }
 }
-
-
-    
-
-    
